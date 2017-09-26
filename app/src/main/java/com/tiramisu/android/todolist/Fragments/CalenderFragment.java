3f174c1788d40bea1180694b3e6b40353769f43a
SharmadDesai <siddhantk05@gmail.com>
2017-09-26 23:17:27 +0530

    still unstable calendar

--- app/src/main/java/com/tiramisu/android/todolist/Fragments/CalenderFragment.java
+++ app/src/main/java/com/tiramisu/android/todolist/Fragments/CalenderFragment.java
@@ -1,10 +1,10 @@
 package com.tiramisu.android.todolist.Fragments;
 
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.constraint.solver.widgets.Snapshot;
+
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,63 +15,60 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.imanoweb.calendarview.CustomCalendarView;
-import com.stacktips.view.DayDecorator;
-import com.stacktips.view.DayView;
-import com.stacktips.view.utils.CalendarUtils;
+
+
+import com.roomorama.caldroid.CaldroidFragment;
+import com.roomorama.caldroid.CaldroidListener;
+
 import com.tiramisu.android.todolist.Model.StaticVar;
 import com.tiramisu.android.todolist.R;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-import java.util.Locale;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.util.Calendar;
+
+
+
 public class CalenderFragment extends Fragment {
 
-    com.imanoweb.calendarview.CustomCalendarView calendarView;
+
     private DatabaseReference calref;
+    int counter =0;
 
     public CalenderFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup view,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup view, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_calender, view, false);
         calref = FirebaseDatabase.getInstance().getReference("Todo/"+ StaticVar.UID +"/Categories");
 
-        calendarView = (CustomCalendarView)rootView.findViewById(R.id.calendar_view);
-        Calendar currentCalendar = Calendar.getInstance(Locale.getDefault());
-
-        calendarView.setFirstDayOfWeek(Calendar.MONDAY);
 
         calref.keepSynced(true);
 
         calref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d("cremeux4", ""+dataSnapshot.getChildrenCount());
 
-
-                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-
-
+                for(final DataSnapshot snapshot : dataSnapshot.getChildren()){
 
                     Log.d("cremeux1", ""+snapshot.getValue());
+                    Log.d("cremeux3", ""+snapshot.getChildrenCount());
+
 
                     if(snapshot.child("Tasks")!= null)
                     {
                         snapshot.child("Tasks").getRef().addValueEventListener(new ValueEventListener() {
                             @Override
                             public void onDataChange(DataSnapshot dataSnapshot) {
+                                Log.d("cremCount", ""+snapshot.child("Tasks").getChildrenCount());
+
                                 for(DataSnapshot snapshot2 : dataSnapshot.getChildren()){
 
                                     Log.d("cremeux2", ""+snapshot2.child("dueDate").getValue().toString());
+                                    counter++;
 
                                 }
 
@@ -84,8 +81,6 @@ public class CalenderFragment extends Fragment {
                         });
                     }
 
-
-
                 }
 
             }
@@ -96,22 +91,19 @@ public class CalenderFragment extends Fragment {
             }
         });
 
-        List decorators = new ArrayList<>();
-        decorators.add(new ColorDecorator() );
-        calendarView.setDecorators(decorators);
-        calendarView.(currentCalendar);
+
+        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        Bundle args = new Bundle();
+        Calendar cal = Calendar.getInstance();
+        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        caldroidFragment.setArguments(args);
+
+        FragmentTransaction t = getFragmentManager().beginTransaction();
+        t.replace(R.id.calender1, caldroidFragment);
+        t.commit();
+
 
         return rootView;
     }
-
-    private class ColorDecorator implements DayDecorator {
-        @Override
-        public void decorate(DayView dayView) {
-            if (CalendarUtils.isToday(Calendar.getInstance())) {
-                int color = Color.parseColor("#a9afb9");
-                dayView.setBackgroundColor(color);
-            }
-        }
-    }
-
 }
